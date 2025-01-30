dd3bdf6bb3d7a3b81ca8c5178f917b73808677ef
Yagiz <yagizemreonat@gmail.com>
2025-01-30 20:49:00 +0200

    Commit

---
+++ 27-01-2025 - 14. -Advanced Functions - Exercise/06.JS-Advanced-Advanced-Functions-Exercise-Resources/03.Fibonacci/03. Fibonacci.js
@@ -0,0 +1,40 @@
+function fibonacci() {
+  let num1 = 0;
+  let num2 = 1;
+
+  return function fib() {
+    let result = num1 + num2;
+    num1 = num2;
+
+
+
+    
+    num2 = result;
+    return num1;
+  };
+}
+
+let fib = fibonacci();
+
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
+
+// Fibonacci
+
+// Write a JS function that when called, returns the next Fibonacci number, starting at 0, 1.
+// Use a closure to keep the current number.
+
+// Input
+// There will be no input.
+
+// Output
+// The output must be a Fibonacci number and must be returned from the function.
+
+// Examples
+// Sample execution
+// let fib = getFibonator()
